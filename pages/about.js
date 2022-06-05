84d21ef7439bc05edc95548ff5a4fd1623dd6e6b
sky1991 <sky.dfo@gmail.com>
2022-06-05 23:24:01 +0430

    bigger intro svg

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Icon from '../components/Icon'
 
 const about = () => {
   return (
-    <div className='about' style={{marginTop : '10rem'}}>
+    <div className='about' style={{marginTop : '8rem'}}>
       <Icon/>
     </div>
   )
