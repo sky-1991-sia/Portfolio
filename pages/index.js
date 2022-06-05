8d8a614981c965ea128e5e08e818ba2bf432242d
sky1991 <sky.dfo@gmail.com>
2022-06-05 23:26:29 +0430

    bigger intro svg

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Icon from '../components/Icon'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.Main}>
-      <h2>this is HOME page</h2>
+      <Icon/>
     </main>
   )
 }
