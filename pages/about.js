f18bc788115b1282221f26508895d57000b53048
sky1991 <sky.dfo@gmail.com>
2022-06-10 22:43:06 +0430

    intro second try
    intro based on design developed just for mobile filrst / needs to refactor and make component responsive

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import SkillsIntroSvg from '../components/SkillsIntroSvg'
 const about = () => {
   return (
     <main className='about' style={{marginTop : '8rem'}}>
-      <SkillsIntroSvg/>
+     <h2>About</h2>
     </main>
   )
 }
