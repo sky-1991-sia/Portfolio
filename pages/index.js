7f683450fbac3d02f56b28270471d6b4d9e8a633
sky1991 <sky.dfo@gmail.com>
2022-06-27 16:15:28 +0430

    try to fix production bug after using BS

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Intro from '../components/Intro'
 import ArticlesShowcase from '../components/ArticlesShowcase'
 import SkillCards from '../components/skillCards/SkillCards'
 import SkillsIntroSvg from '../components/SkillsIntroSvg'
+import Head from 'next/head'
+import Script from "next/script";
 //styles
 import styles from '../styles/Home.module.scss'
 // animate
@@ -21,7 +23,13 @@ export default function Home() {
        AOS.init();
     })
   return (
-    <main className={styles.Main}>
+    <>
+      <Head>
+        {/* BOOTSTRAP */}
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" ></link>
+        {/* BOOTSTRAP */}
+      </Head>
+      <main className={styles.Main}>
       <section className={styles.Main__IntroCon}>
       <SkillsIntroSvg/>
       <Intro/>
@@ -34,5 +42,11 @@ export default function Home() {
       <ScrollTopBtn/>
       <FutureSkills/>
     </main>
+    {/* BOOTSTRAP */}
+    <Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"/>
+    <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"/>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"/>
+    {/* BOOTSTRAP */}
+    </>
   )
 }
