ee74d84b987d55e081bb05bafde2d49d62032091
sky1991 <sky.dfo@gmail.com>
2022-06-27 17:11:09 +0430

    try to fix production bug after using BS

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
       <title>portfolio | skyde</title>
       <meta name="description" content="React/Next js developer who is interested about ui/ux and backend at same time" />
       <meta name="google-site-verification" content="y5Ys5_Mrd_5R4boIGfbtRcYWE26lPceBg1TgxQzYyPw" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" ></link>
     </Head>
     <Header/>
     <Component {...pageProps} />
